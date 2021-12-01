7501ecfa39de314a4bc3023a1979cf14a1038e0f
Dmitry Bezrodny <85192800+dmitrybezrodny17@users.noreply.github.com>
2021-12-01 09:38:12 +0200

    Delete settings.py

--- modules/settings.py
+++
@@ -1,7 +0,0 @@
-TOKEN = "AAAAAAAAAAAAAAAAAAAAALofUgEAAAAAQKqm4w1f%2FQ3gEdq2QcYvO4oF3xQ%3DthLM0LF4jNuuzFy8GF9yE7AcmhTgk90v1ZEMRdD4vRHXcHCjxS"
-
-client_id = "tvC9dmt_MPORiuWOXvUY7w"
-client_secret = "Ht6U8SouHWDHez-MkIOMHCdWAMXwDg"
-password = "xvt192nv2r"
-user_agent = "crypto-mentions"
-username = "IvashkovMG"
